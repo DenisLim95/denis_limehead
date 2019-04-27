8354e7c41ed3287459f071fb5aee7463ff8a5f82
Denis Lim <denislim@usc.edu>
2019-04-26 22:52:41 -0700

    added music

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.render('landing');
 });
 
+app.get("/music", function(req, res) {
+    res.render("music");
+ });
+
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!');
 });
